a24da9e61231cd1e59c05efef77952d405454b1d
Edwin Marte <edwin.marte@leidba.com>
2018-08-16 14:18:13 -0400

    Simple Hello World
    This is a simple Hello World Test on Arty S7 with Microblaze and Vivado

---
+++ ArtyS7MarteReview/SimpleHelloWorld/Review_01.srcs/sources_1/bd/design_1/ip/design_1_clk_wiz_0_0/design_1_clk_wiz_0_0_stub.v
@@ -0,0 +1,23 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+// Date        : Mon Aug 13 22:02:52 2018
+// Host        : DESKTOP-FIO094F running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/E.Marte/Documents/Vivado_Proyects/Review_01/Review_01.srcs/sources_1/bd/design_1/ip/design_1_clk_wiz_0_0/design_1_clk_wiz_0_0_stub.v
+// Design      : design_1_clk_wiz_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7s50csga324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module design_1_clk_wiz_0_0(clk_out1, clk_out2, resetn, locked, clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_out2,resetn,locked,clk_in1" */;
+  output clk_out1;
+  output clk_out2;
+  input resetn;
+  output locked;
+  input clk_in1;
+endmodule
